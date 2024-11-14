68401bead8e2246b65337f8193e7eccea6c5fc60
melwin <114172287+itzTedx@users.noreply.github.com>
2024-11-14 16:58:48 +0400

    all done final v1 product

--- src/app/(dashboard)/card/[id]/_components/card-form.tsx
+++ src/app/(dashboard)/card/[id]/_components/card-form.tsx
@@ -13,12 +13,14 @@ import {
   IconDots,
   IconEdit,
   IconExternalLink,
+  IconGripVertical,
   IconPlus,
   IconTrash,
   IconUpload,
   IconUser,
   IconX,
 } from "@tabler/icons-react";
+import { Reorder } from "framer-motion";
 import { useAction } from "next-safe-action/hooks";
 import { useFieldArray, useForm, useWatch } from "react-hook-form";
 import { toast } from "sonner";
@@ -92,6 +94,8 @@ export default function CardCustomizeForm({
   const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
   const [openPopover, setOpenPopover] = useState(false);
+  const [active, setActive] = useState(0);
+
   const openCompanyModal = useCompanyFormModal((state) => state.openModal);
 
   const searchParams = useSearchParams();
@@ -144,7 +148,7 @@ export default function CardCustomizeForm({
     control: form.control,
   });
 
-  const { fields, append, remove } = useFieldArray({
+  const { fields, append, remove, move } = useFieldArray({
     name: "links",
     control: form.control,
   });
@@ -656,175 +660,225 @@ export default function CardCustomizeForm({
                 )}
               </TabsContent>
               <TabsContent value="links" className="flex flex-col gap-8">
-                <div className="w-full space-y-4">
+                <Reorder.Group
+                  as="div"
+                  className="w-full space-y-4"
+                  values={fields}
+                  onReorder={(e) => {
+                    const activeEl = fields[active];
+                    e.map((item, index) => {
+                      if (item === activeEl) {
+                        move(active, index);
+                        setActive(index);
+                        return;
+                      }
+                      return;
+                    });
+                  }}
+                >
                   {fields.map((data, index) =>
                     data.category === "General" ? (
-                      <Card
+                      <Reorder.Item
+                        as="div"
+                        id={data.id}
+                        onDragStart={() => setActive(index)}
+                        value={data}
                         key={data.id}
-                        className="flex items-center justify-between gap-6 p-4"
                       >
-                        <div className="grid grid-cols-5 items-center gap-4">
-                          <FormField
-                            control={form.control}
-                            name={`links.${index}.label`}
-                            render={({ field }) => (
-                              <FormItem className="col-span-2 flex gap-3 space-y-0">
-                                <Image
-                                  src={data.icon}
-                                  height={40}
-                                  width={40}
-                                  alt=""
-                                  className="flex-shrink-0"
-                                />
-                                <FormControl>
-                                  <Input
-                                    className="space-y-0"
-                                    {...field}
-                                    disabled={loading}
-                                    placeholder={`${data.label} Title`}
+                        <Card className="flex items-center justify-between gap-2 p-4">
+                          <div className="grid grid-cols-5 items-center gap-4">
+                            <FormField
+                              control={form.control}
+                              name={`links.${index}.label`}
+                              render={({ field }) => (
+                                <FormItem className="col-span-2 flex gap-3 space-y-0">
+                                  <Image
+                                    src={data.icon}
+                                    height={40}
+                                    width={40}
+                                    alt=""
+                                    className="flex-shrink-0"
                                   />
-                                </FormControl>
-                                <FormMessage />
-                              </FormItem>
-                            )}
-                          />
-                          <FormField
-                            control={form.control}
-                            key={data.id}
-                            name={`links.${index}.url`}
-                            render={({ field }) => (
-                              <FormItem className="col-span-3">
-                                <FormControl>
-                                  <Input
-                                    {...field}
-                                    disabled={loading}
-                                    placeholder={`${data.label} Url`}
-                                  />
-                                </FormControl>
-                                <FormMessage />
-                              </FormItem>
-                            )}
-                          />
-                        </div>
-                        <Button
-                          type="button"
-                          variant="ghost"
-                          size="icon"
-                          onClick={() => remove(index)}
-                        >
-                          <IconTrash size={16} />
-                        </Button>
-                      </Card>
+                                  <FormControl>
+                                    <Input
+                                      className="space-y-0"
+                                      {...field}
+                                      disabled={loading}
+                                      placeholder={`${data.label} Title`}
+                                    />
+                                  </FormControl>
+                                  <FormMessage />
+                                </FormItem>
+                              )}
+                            />
+                            <FormField
+                              control={form.control}
+                              key={data.id}
+                              name={`links.${index}.url`}
+                              render={({ field }) => (
+                                <FormItem className="col-span-3">
+                                  <FormControl>
+                                    <Input
+                                      {...field}
+                                      disabled={loading}
+                                      placeholder={`${data.label} Url`}
+                                    />
+                                  </FormControl>
+                                  <FormMessage />
+                                </FormItem>
+                              )}
+                            />
+                          </div>
+                          <Button
+                            type="button"
+                            variant="ghost"
+                            className="text-destructive hover:text-destructive"
+                            size="icon"
+                            onClick={() => remove(index)}
+                          >
+                            <IconTrash size={16} />
+                          </Button>
+                          <Button
+                            type="button"
+                            variant="ghost"
+                            size="icon"
+                            title="Drag to Re-Order"
+                            className="text-gray-400 hover:bg-transparent hover:text-foreground"
+                            onClick={(e) => e.preventDefault()}
+                          >
+                            <IconGripVertical size={16} />
+                          </Button>
+                        </Card>
+                      </Reorder.Item>
                     ) : (
-                      <Card
+                      <Reorder.Item
+                        as="div"
+                        id={data.id}
+                        onDragStart={() => setActive(index)}
+                        value={data}
                         key={data.id}
-                        className="flex items-center justify-between gap-6 p-4"
                       >
-                        <div className="flex flex-1 gap-3">
-                          <Image
-                            src={data.icon}
-                            height={40}
-                            width={40}
-                            alt=""
-                          />
-                          <FormField
-                            control={form.control}
-                            key={data.id}
-                            name={`links.${index}.url`}
-                            render={({ field }) => (
-                              <FormItem className="w-full">
-                                <FormControl>
-                                  <Input
-                                    {...field}
-                                    disabled={loading}
-                                    placeholder={`${data.label} url`}
-                                  />
-                                </FormControl>
-
-                                <FormMessage />
-                              </FormItem>
-                            )}
-                          />
-                        </div>
-                        <Button
-                          type="button"
-                          variant="ghost"
-                          size="icon"
-                          onClick={() => remove(index)}
+                        <Card
+                          key={data.id}
+                          className="flex items-center justify-between gap-2 p-4"
                         >
-                          <IconTrash size={16} />
-                        </Button>
-                      </Card>
+                          <div className="flex flex-1 gap-3">
+                            <Image
+                              src={data.icon}
+                              height={40}
+                              width={40}
+                              alt=""
+                            />
+                            <FormField
+                              control={form.control}
+                              key={data.id}
+                              name={`links.${index}.url`}
+                              render={({ field }) => (
+                                <FormItem className="w-full">
+                                  <FormControl>
+                                    <Input
+                                      {...field}
+                                      disabled={loading}
+                                      placeholder={`${data.label} url`}
+                                    />
+                                  </FormControl>
+
+                                  <FormMessage />
+                                </FormItem>
+                              )}
+                            />
+                          </div>
+                          <Button
+                            type="button"
+                            variant="ghost"
+                            size="icon"
+                            className="text-destructive hover:text-destructive"
+                            onClick={() => remove(index)}
+                          >
+                            <IconTrash size={16} />
+                          </Button>
+                          <Button
+                            type="button"
+                            variant="ghost"
+                            size="icon"
+                            title="Drag to Re-Order"
+                            className="text-gray-400 hover:bg-transparent hover:text-foreground"
+                            onClick={(e) => e.preventDefault()}
+                          >
+                            <IconGripVertical size={16} />
+                          </Button>
+                        </Card>
+                      </Reorder.Item>
                     )
                   )}
-                  <Dialog open={open} onOpenChange={setOpen}>
-                    <DialogTrigger asChild>
-                      <Button
-                        type="button"
-                        variant="outline"
-                        className="h-12 w-full"
-                      >
-                        <IconPlus size={16} className="mr-2" /> Add
-                      </Button>
-                    </DialogTrigger>
-                    <DialogContent className="max-w-2xl p-0">
-                      <DialogHeader className="border-b p-6">
-                        <DialogTitle>Add Link</DialogTitle>
-                        <DialogDescription className="sr-only">
-                          Add Social or custom links to digital card
-                        </DialogDescription>
-                      </DialogHeader>
-                      <div className="p-6 pb-6 pt-0">
-                        {LINKS.map((item, i) => (
-                          <div key={i} className="py-3">
-                            <h4 className="pb-2 text-sm font-medium text-gray-700">
-                              {item.label}
-                            </h4>
+                </Reorder.Group>
+                <Dialog open={open} onOpenChange={setOpen}>
+                  <DialogTrigger asChild>
+                    <Button
+                      type="button"
+                      variant="outline"
+                      className="h-12 w-full"
+                    >
+                      <IconPlus size={16} className="mr-2" /> Add
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent className="max-w-2xl p-0">
+                    <DialogHeader className="border-b p-6">
+                      <DialogTitle>Add Link</DialogTitle>
+                      <DialogDescription className="sr-only">
+                        Add Social or custom links to digital card
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className="p-6 pb-6 pt-0">
+                      {LINKS.map((item, i) => (
+                        <div key={i} className="py-3">
+                          <h4 className="pb-2 text-sm font-medium text-gray-700">
+                            {item.label}
+                          </h4>
 
-                            <div className="grid grid-cols-[repeat(auto-fill,minmax(18rem,1fr))] gap-x-6">
-                              {item.links.map((link, i) => (
-                                <div
-                                  className="flex items-center justify-between border-b py-3"
-                                  key={`addLink-${i}-${link.label}`}
-                                >
-                                  <div className="flex items-center gap-4 font-medium">
-                                    <div className="relative size-8">
-                                      <Image
-                                        src={link.icon}
-                                        fill
-                                        alt=""
-                                        sizes="10vw"
-                                      />
-                                    </div>
-
-                                    <p>{link.label}</p>
+                          <div className="grid grid-cols-[repeat(auto-fill,minmax(18rem,1fr))] gap-x-6">
+                            {item.links.map((link, i) => (
+                              <div
+                                className="flex items-center justify-between border-b py-3"
+                                key={`addLink-${i}-${link.label}`}
+                              >
+                                <div className="flex items-center gap-4 font-medium">
+                                  <div className="relative size-8">
+                                    <Image
+                                      src={link.icon}
+                                      fill
+                                      alt=""
+                                      sizes="10vw"
+                                    />
                                   </div>
-                                  <DialogClose asChild>
-                                    <Button
-                                      variant="ghost"
-                                      onClick={() =>
-                                        append({
-                                          category: item.label,
-                                          label: link.label,
-                                          url: link.url,
-                                          icon: link.icon,
-                                        })
-                                      }
-                                      className="h-8 gap-2 px-2 font-semibold text-primary hover:text-blue-800"
-                                    >
-                                      <IconPlus className="size-3 stroke-[2.5]" />
-                                      Add
-                                    </Button>
-                                  </DialogClose>
+
+                                  <p>{link.label}</p>
                                 </div>
-                              ))}
-                            </div>
+                                <DialogClose asChild>
+                                  <Button
+                                    variant="ghost"
+                                    onClick={() =>
+                                      append({
+                                        category: item.label,
+                                        label: link.label,
+                                        url: link.url,
+                                        icon: link.icon,
+                                      })
+                                    }
+                                    className="h-8 gap-2 px-2 font-semibold text-primary hover:text-blue-800"
+                                  >
+                                    <IconPlus className="size-3 stroke-[2.5]" />
+                                    Add
+                                  </Button>
+                                </DialogClose>
+                              </div>
+                            ))}
                           </div>
-                        ))}
-                      </div>
-                    </DialogContent>
-                  </Dialog>
-                </div>
+                        </div>
+                      ))}
+                    </div>
+                  </DialogContent>
+                </Dialog>
 
                 <section>
                   <div className="flex items-center justify-between">
