c2801004ee096a54126ba10a426d81ad971a2f80
melwin <114172287+itzTedx@users.noreply.github.com>
2025-02-28 01:17:35 +0400

    feat: enable experimental package import optimization for @tabler/icons-react

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import withPlaiceholder from "@plaiceholder/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: { optimizePackageImports: ["@tabler/icons-react"] },
   typescript: {
     ignoreBuildErrors: true,
   },
